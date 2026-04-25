68e1516f6b71773254edcd642bd0e9cc16e09c6f
Adekunle Adegbie <kadegbie@gmail.com>
2026-04-25 10:46:50 +0100

    Fix one file

--- utils/emailer.py
+++ utils/emailer.py
@@ -6,13 +6,13 @@ def send_email_to_user(email, subject, body):
 
     msg = MIMEText(body)
     msg["Subject"] = subject
-    msg["From"] = st.secrets["EMAIL_USER"]
+    msg["From"] = os.getenv["EMAIL_USER"]
     msg["To"] = email
 
     try:
-        server = smtplib.SMTP(st.secrets["EMAIL_HOST"], st.secrets["EMAIL_PORT"])
+        server = smtplib.SMTP(os.getenv["EMAIL_HOST"], os.getenv["EMAIL_PORT"])
         server.starttls()
-        server.login(st.secrets["EMAIL_USER"], st.secrets["EMAIL_PASS"])
+        server.login(os.getenv["EMAIL_USER"], os.getenv["EMAIL_PASS"])
         server.sendmail(msg["From"], [msg["To"]], msg.as_string())
         server.quit()
     except Exception as e:
